0c7211cd15769873de529481618ba0e6f68e0f6c
ImFineGarrix <chaon55526@gmail.com>
2023-12-10 03:59:40 +0700

    :sparkles: search and check

--- components/EmptyData.vue
+++ components/EmptyData.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <p class="mt-24 text-2xl text-center text-gray-500">เร็ว ๆ นี้</p>
+    <p class="mt-24 text-2xl text-center text-gray-500" v-if="active">
+      เร็ว ๆ นี้
+    </p>
+    <p class="mt-24 text-2xl text-center text-gray-500" v-else>ไม่พบรายการ</p>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      deafult: () => true,
+    },
+  },
+}
+</script>
--- pages/skills/index.vue
+++ pages/skills/index.vue
@@ -4,7 +4,9 @@
       <div class="space-y-7">
         <p class="text-6xl font-semibold text-center font-poppin">SKILLS</p>
         <div class="space-y-4">
-          <Search @input="handleSearch" placeholder="ค้นหาทักษะคุณสนใจ" />
+          <Search
+            @update-search="handleSearch"
+            placeholder="ค้นหาทักษะคุณสนใจ" />
         </div>
       </div>
     </div>
@@ -21,54 +23,49 @@
     </div>
     <div>
       <p class="text-2xl font-semibold">ทักษะทั้งหมด</p>
-      <div class="grid grid-cols-4 gap-4 my-12" v-if="Check.checkEmpty(skills)">
-        <div v-for="(skill, indexSkill) in skills" :key="`skill-${indexSkill}`">
-          <NuxtLink :to="`/skills/${skill.skillId}`">
-            <CardSkill
-              :name="skill.name"
-              :desc="skill.description"
-              :level="skill.levelId"
-              :image="`${config.public.firebaseBaseUrl}${skill.imageUrl}`" />
-          </NuxtLink>
+      <div v-if="ready">
+        <div
+          class="grid grid-cols-4 gap-4 my-12"
+          v-if="Composables.check.checkEmpty(skills)">
+          <div
+            v-for="(skill, indexSkill) in searchSkill"
+            :key="`skill-${indexSkill}`">
+            <NuxtLink :to="`/skills/${skill.skillId}`">
+              <CardSkill
+                :name="skill.name"
+                :desc="skill.description"
+                :level="skill.levelId"
+                :image="`${config.public.firebaseBaseUrl}${skill.imageUrl}`" />
+            </NuxtLink>
+          </div>
         </div>
+        <EmptyData v-else />
       </div>
-      <EmptyData v-else />
+      <Loading v-else />
     </div>
   </div>
 </template>
 
 <script>
 import { useRuntimeConfig } from 'nuxt/app'
-import UseCheck from '~/composables/check'
+import { MainComposables } from '~/composables/index'
 import SkillProvider from '~/resources/SkillProvider'
 export default {
   data() {
     return {
       SkillService: new SkillProvider(),
-      Check: new UseCheck(),
-      search: '1',
+      Composables: MainComposables(),
+      search: '',
       skills: [],
       config: useRuntimeConfig(),
-      recommendSkill: [
-        {
-          name: 'Coding',
-          link: '/images/icon/good-code.png',
-        },
-        {
-          name: 'Coding',
-          link: '/images/icon/good-code.png',
-        },
-        {
-          name: 'Coding',
-          link: '/images/icon/good-code.png',
-        },
-        {
-          name: 'Coding',
-          link: '/images/icon/good-code.png',
-        },
-      ],
       skills: [],
+      ready: false,
     }
+  },
+  computed: {
+    searchSkill() {
+      return this.Composables.search.searchByText(this.skills, this.search)
+    },
   },
   mounted() {
     this.getSkill()
@@ -78,13 +75,15 @@ export default {
       const status = await this.SkillService.getSkill()
       if (status.message === 'success') {
         this.skills = status.data.skills
+        this.ready = true
       }
     },
     handleSearch(newSearch) {
-      this.search = newSearch.target.value
+      this.search = newSearch
     },
   },
 }
 </script>
 
 <style lang="scss" scoped></style>
+~/composables/modules/check
